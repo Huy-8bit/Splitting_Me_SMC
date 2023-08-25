f99273fe3d894a9d99b4980fd0704e77f4d83c1d
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2023-08-25 14:48:18 +0700

    udpate

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -59,41 +59,41 @@ contract TokenSale is Ownable {
         }
     }
 
-    function buyPackageWithReferral(
-        string memory _packageName,
-        address _referrer
-    ) external payable {
-        require(packages[_packageName].price > 0, "Invalid package name");
+    // function buyPackageWithReferral(
+    //     string memory _packageName,
+    //     address _referrer
+    // ) external payable {
+    //     require(packages[_packageName].price > 0, "Invalid package name");
 
-        uint256 ethAmount = packages[_packageName].price;
-        uint256 tokensToReceive = packages[_packageName].tokens;
-        uint256 commissionPercentage = packages[_packageName]
-            .commissionPercentage;
+    //     uint256 ethAmount = packages[_packageName].price;
+    //     uint256 tokensToReceive = packages[_packageName].tokens;
+    //     uint256 commissionPercentage = packages[_packageName]
+    //         .commissionPercentage;
 
-        // Calculate referral commission and update referral mapping
-        if (_referrer != address(0) && _referrer != msg.sender) {
-            tokensToReceive += (tokensToReceive * commissionPercentage) / 100;
-            referrals[msg.sender] = _referrer;
-        }
+    //     // Calculate referral commission and update referral mapping
+    //     if (_referrer != address(0) && _referrer != msg.sender) {
+    //         tokensToReceive += (tokensToReceive * commissionPercentage) / 100;
+    //         referrals[msg.sender] = _referrer;
+    //     }
 
-        // Transfer tokens to the buyer
-        require(msg.value >= ethAmount, "Insufficient ether sent");
-        token.transfer(msg.sender, tokensToReceive);
+    //     // Transfer tokens to the buyer
+    //     require(msg.value >= ethAmount, "Insufficient ether sent");
+    //     token.transfer(msg.sender, tokensToReceive);
 
-        // Distribute commission to referrer
-        address referrer = referrals[msg.sender];
-        if (referrer != address(0)) {
-            token.transfer(
-                referrer,
-                (tokensToReceive * commissionPercentage) / 100
-            );
-        }
+    //     // Distribute commission to referrer
+    //     address referrer = referrals[msg.sender];
+    //     if (referrer != address(0)) {
+    //         token.transfer(
+    //             referrer,
+    //             (tokensToReceive * commissionPercentage) / 100
+    //         );
+    //     }
 
-        // Refund excess ether
-        if (msg.value > ethAmount) {
-            payable(msg.sender).transfer(msg.value - ethAmount);
-        }
-    }
+    //     // Refund excess ether
+    //     if (msg.value > ethAmount) {
+    //         payable(msg.sender).transfer(msg.value - ethAmount);
+    //     }
+    // }
 
     function setPackage(
         string memory _packageName,
